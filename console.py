bee330737fae104498312b4582b4452c82055cd6
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-21 11:57:36 +0300

    Update do_create to accept params

--- console.py
+++ console.py
@@ -114,17 +114,44 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+
+        # split the args to get the class name
+        class_name = args.split(' ')[0]
+        # Validate if the class name is provided and if it exists
+        if not class_name:
             print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes.keys():
             print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        else:
+            # create an instance of the class
+            new_model = HBNBCommand.classes[class_name]()
+
+            # if there are parameters provided, set the attributes
+            if len(args.split(' ')) > 1:
+
+                # get the parameters from the string
+                params = args.split(' ')[1:]
+                for param in params:
+                    # split the param to get the key and value
+                    key, value = param.split('=')
+
+                    # if value is convertable to another type, convert it
+                    # convertable types are int, float, bool
+                    try:
+                        value = eval(value)
+                    except Exception as e:
+                        pass
+                    # if value is a string and it contains underscore(_)
+                    # replace the underscore with space
+                    if type(value) is str and '_' in value:
+                        value = value.replace('_', ' ')
+
+                    setattr(new_model, key, value)
+                new_model.save()
+                print(new_model.id)
+                return
+            new_model.save()
+            print(new_model.id)
 
     def help_create(self):
         """ Help information for the create method """
