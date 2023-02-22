51dd413c7dc2375d1ed20d124d1c99eb4463a0f0
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-22 13:49:16 +0300

    Fix typo

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbtn_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- grant select privileges to hbtn_dev on the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'; 
