e0eb4ded22790fa60e52ce4f36411adf37351f89
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-23 16:33:57 +0300

    Add State Model tables

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+# import sqlalchemy modules
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import Base
 
-
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(Base, BaseModel):
+    """ State model class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    # create a relationship with cities
+    cities = relationship('City', back_populates='state', cascade='all, delete')
