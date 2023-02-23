2fe63e5d6955b6b9a2459ed5e83e5cbb06234e81
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-23 16:36:37 +0300

    Fix to meet pep8

--- models/state.py
+++ models/state.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 # import sqlalchemy modules
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import Base
 
 class State(Base, BaseModel):
     """ State model class """
