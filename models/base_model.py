30587af9253fe1edcb43fdc8c15888a83a5867f1
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 02:30:53 +0300

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -19,32 +19,39 @@ storage_type = 'HBNB_TYPE_STORAGE'
 class BaseModel:
     """A base class for all hbnb models"""
 
-    if storage_type in environ.keys() and environ.get('HBNB_TYPE_STORAGE') == 'db':
-        id = Column(String(60), primary_key=True, nullable=False,
-                    default=str(uuid.uuid4()))
-        created_at = Column(DateTime, nullable=False,
-                            default=datetime.utcnow())
-        updated_at = Column(DateTime, nullable=False,
-                            default=datetime.utcnow())
+    
+    id = Column(String(60), primary_key=True, nullable=False,
+                default=str(uuid.uuid4()))
+    created_at = Column(DateTime, nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False,
+                        default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if storage_type not in environ.keys() or environ["HBNB_TYPE_STORAGE"] != "db":
-
-            if not kwargs:
-                from models import storage
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
                 self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
-            else:
-                for key, val in kwargs.items():
-                    if key == '__class__':
-                        continue
-                    if key == 'created_at':
-                        val = datetime.fromisoformat(val)
-                    elif key == 'updated_at':
-                        val = datetime.fromisoformat(val)
-                    self.__setattr__(key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
             # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
             #                                          '%Y-%m-%dT%H:%M:%S.%f')
