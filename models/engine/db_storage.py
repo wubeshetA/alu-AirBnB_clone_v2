f65ebfedad472bd9d3b7d6479f3158fb301020b9
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-23 16:07:36 +0300

    Add methods for db operation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -30,5 +30,40 @@ class DBStorage:
         if env == 'test':
             Base.metadata.drop_all(self.__engine)
     
+    def all(self, cls=None):
+        """ query on the current database session """
+        # create a dictionary
+        obj_dict = {}
+        if cls is None:
+            for table in Base.metadata.tables:
+                for obj in self.__session.query(table):
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    obj_dict[key] = obj
+        else:
+            for obj in self.__session.query(cls):
+                key = obj.__class__.__name__ + '.' + obj.id
+                obj_dict[key] = obj
+        return obj_dict
     
+    def new(self, obj):
+        """ add the object to the current database session """
+        self.__session.add(obj)
+    
+    def save(self):
+        """ commit all changes of the current database session """
+        self.__session.commit()
+    
+    def delete(self, obj=None):
+        """ delete from the current database session obj if not None """
+        if obj is not None:
+            self.__session.delete(obj)
+    
+    def reload(self):
+        """ create all tables in the database """
+        Base.metadata.create_all(self.__engine)
+        # create a configured "Session" class
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        # create a Session
+        self.__session = Session()
 
+        
