74bc40576d56c7260ede84a587761e1335d7f48b
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 02:31:24 +0300

    Update City model

--- models/city.py
+++ models/city.py
@@ -5,20 +5,12 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
 from os import environ
 
-storage_type = 'HBNB_TYPE_STORAGE'
-
-if storage_type in environ.keys() and environ.get('HBNB_TYPE_STORAGE') == 'db':
-    class City(BaseModel, Base):
-        """ The city class, contains state ID and name """
-
-        __tablename__ = 'cities'
-
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-else:
-    class City(BaseModel):
-        """ The city class, contains state ID and name """
-
-        state_id = ""
-        name = ""
+class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
