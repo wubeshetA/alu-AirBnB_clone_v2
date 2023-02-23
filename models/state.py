622b02c30967978d527d77a680a26d68b1e72602
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 02:32:00 +0300

    Update state model

--- models/state.py
+++ models/state.py
@@ -5,9 +5,15 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class State(Base, BaseModel):
-    """ State model class """
-    __tablename__ = 'states'
+
+class State(BaseModel, Base):
+
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+
     name = Column(String(128), nullable=False)
-    # create a relationship with cities
-    cities = relationship('City', back_populates='state', cascade='all, delete')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
