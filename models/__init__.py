43d26426640a9e872b4e266c67b77be241d0ca15
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 02:30:15 +0300

    Add import os

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-import os
 from models.engine.db_storage import DBStorage
+import os
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
