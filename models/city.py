2cfe571d51d819aff822549d28e586457582cea1
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-23 23:37:00 +0300

    Update city.py to consider both storage type

--- models/city.py
+++ models/city.py
@@ -3,12 +3,22 @@
 from models.base_model import BaseModel, Base
 # import sqlalchemy modules
 from sqlalchemy import Column, ForeignKey, String
+from os import environ
 
+storage_type = 'HBNB_TYPE_STORAGE'
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+if storage_type in environ.keys() and environ.get('HBNB_TYPE_STORAGE') == 'db':
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
 
-    __tablename__ = 'cities'
+        __tablename__ = 'cities'
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+else:
+    class City(BaseModel):
+        """ The city class, contains state ID and name """
+
+        state_id = ""
+        name = ""
