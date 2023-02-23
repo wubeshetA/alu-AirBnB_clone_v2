647f951ae62a4fc04f9ff3520f10f7439d4e16c1
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-23 16:31:01 +0300

    add Base var import

--- models/city.py
+++ models/city.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 # import sqlalchemy modules
-from sqlalchemy import Column, ForeignKey, Integer, String
-from models.base_model import Base
+from sqlalchemy import Column, ForeignKey, String
 
 
 class City(BaseModel, Base):
