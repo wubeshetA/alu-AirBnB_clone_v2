c3a5fc18461ca370700eabac50f18434f300f919
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 20:58:16 +0300

    Remove data type of place_id and user_id

--- models/review.py
+++ models/review.py
@@ -13,6 +13,6 @@ class Review(BaseModel, Base):
     """
     __tablename__ = "reviews"
 
-    place_id = Column(Integer, ForeignKey('places.id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    place_id = Column(ForeignKey('places.id'), nullable=False)
+    user_id = Column(ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
