1d882ed24d6768bc20661e5629c97c99804626c2
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 15:53:08 +0300

    Add amenity model

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+
+    """Amenity class to store amenity information"""
+
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship("Place", secondary="place_amenity")
+
+    def __init__(self, *args, **kwargs):
+        """initializes amenity"""
+        super().__init__(*args, **kwargs)
