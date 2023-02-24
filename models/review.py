2d4bfe08d24cdbda4b8be2db7230bbe6c3fc59b7
Wubeshet Yimam <w.yimam@alustudent.com>
2023-02-24 03:29:54 +0300

    Add table name

--- models/review.py
+++ models/review.py
@@ -3,8 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 
+
 class Review(BaseModel, Base):
     """ Review classto store review information """
+    __tablename__ = "reviews"
+
     place_id = Column(Integer, ForeignKey('places.id'), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
