182af6e3db0a51cd4c3a0e5654b54421c83825c6
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-10 08:43:06 +0300

    Add file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# generate .tgz archive
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder"""
+    from fabric.api import local
+
+    from datetime import datetime
+    import os
+
+    now = datetime.now()
+    date = now.strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    local("mkdir -p versions")
+    local("tar -cvzf {} web_static".format(filename))
+    if os.path.exists(filename):
+        return filename
+    return None
