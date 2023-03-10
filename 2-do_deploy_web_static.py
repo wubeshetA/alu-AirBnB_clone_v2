86c38265a1ccc258c4d549b54ebea8bd02127a90
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-10 16:46:36 +0300

    Update file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to a web servers"""
 
+from fabric.api import local, env, put, run
+from datetime import datetime
+import os
+
+env.hosts = ['54.197.205.125', '3.80.28.138']
+env.user = 'ubuntu'
+
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
-    from fabric.api import local
-    from datetime import datetime
-    import os
 
     now = datetime.now()
     date = now.strftime("%Y%m%d%H%M%S")
@@ -20,10 +24,9 @@ def do_pack():
 
 def do_deploy(archive_path):
     """distributes an archive to a web servers"""
-    from fabric.api import put, run, env
-    from os.path import exists
 
-    if not exists(archive_path):
+    if not os.path.exists(archive_path):
+
         return False
     try:
         put(archive_path, "/tmp/")
@@ -32,13 +35,13 @@ def do_deploy(archive_path):
         run("mkdir -p /data/web_static/releases/{}/".format(name))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
             filename, name))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* \
+        run("sudo rm /tmp/{}".format(filename))
+        run("sudo cp -rf /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".format(
             name))
         return True
-    except:
+    except Exception:
         return False
