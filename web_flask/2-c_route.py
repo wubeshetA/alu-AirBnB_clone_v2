54ac74c050808fb6399d160a837f77af0ad97e78
wubeshetA <w.yimam@alustudent.com>
2023-03-23 18:13:10 +0300

    Add files

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,6 +9,16 @@ def hello():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    return "C {}".format(text.replace("_", " "))
+
+
 if __name__ == '__main__':
     # start the flask app
     app.run(host='0.0.0.0', port=5000, debug=True)
