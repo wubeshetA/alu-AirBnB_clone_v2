0f976bf68a39e131451208b0d43987f18912d66e
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-24 13:37:35 +0300

    Fix pep8 error

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 # import sqlalchemy modules
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.orderinglist import ordering_list
 
 
 class State(BaseModel, Base):
@@ -13,15 +14,17 @@ class State(BaseModel, Base):
         name: input name
     """
     __tablename__ = "states"
+    # write a code to make BaseModel column order first in the table
 
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
                           backref="state")
+    # write a code to make BaseModel column order first in the table
 
-    # dd a public getter method cities to return the list of City objects from storage linked to the current State
     @property
     def cities(self):
-        """getter attribute cities that returns the list of City objects from storage linked to the current State"""
+        """getter attribute cities that returns the list of City objects
+        from storage linked to the current State"""
         from models import storage
         from models.city import City
         cities = []
