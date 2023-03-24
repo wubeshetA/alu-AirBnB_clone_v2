94e72a2628fee2417efaaca51e50055067ed04a2
Wubeshet Yimam <w.yimam@alustudent.com>
2023-03-24 06:53:36 +0300

    Update db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,17 +2,15 @@
 """ Database storage module """
 from sqlalchemy import create_engine
 # import declarative base
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 from models.amenity import Amenity
-from models.base_model import *
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.city import City
 from models.user import User
-
+from models.base_model import Base
 
 class DBStorage:
 
