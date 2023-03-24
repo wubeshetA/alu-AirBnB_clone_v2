868b93eab24dd06d76a93388e99e7ae5acd56773
wubeshetA <w.yimam@alustudent.com>
2023-03-24 09:28:08 +0300

    Update storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,3 +84,6 @@ class DBStorage:
 
         # Base.metadata.create_all(self.__engine)
         # self.__session = scoped_session(sessionmaker(bind=self.__engine))
+        
+    def close(self):
+        self.__session.remove()
